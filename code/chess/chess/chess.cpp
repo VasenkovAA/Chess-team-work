aff0fb64772efc8c14da43be3614a70bb3021682
Vladimir Sdobnov <vovafigvam@gmail.com>
2023-05-09 21:09:56 +0300

    Проработаны механики хода, съедения и проверки возможности хода

--- code/chess/chess/chess.cpp
+++ code/chess/chess/chess.cpp
@@ -23,28 +23,35 @@ private:
     short int x;
     short int y;
 public:
-    void set_x(int _x) { if (_x > -2 && _x < 8) { x = _x; } else throw invalid_index; }
-    void set_y(int _y) { if (_y > -2 && _y < 8) { y = _y; } else throw invalid_index; }
-    short int get_x() { return x; }
-    short int get_y() { return y; }
-    TCoord& operator=(const TCoord& tmp) {
-        x = tmp.x;
-        y = tmp.y;
-        return *this;
-    }
-    bool operator==(const TCoord& tmp) const {
-        return ((x == tmp.x) && (y == tmp.y));
-    }
-    friend ostream& operator<<(ostream& os, const TCoord& coord);
-    TCoord(short int _x, short int _y) {
-        if (_x < 8 && _x > -2 && _y > -2 && _y < 8) {
-            x = _x;
-            y = _y;
-        }
-        else throw invalid_index;
-    }
+    void set_x(int _x);
+    void set_y(int _y);
+    short int get_x();
+    short int get_y();
+    TCoord& operator=(const TCoord& tmp);
+    bool operator==(const TCoord& tmp) const;
+    TCoord(short int _x, short int _y);
     TCoord() :x(-1), y(-1) {};
+    friend ostream& operator<<(ostream& os, const TCoord& coord);
 };
+void TCoord::set_x(int _x) { if (_x > -2 && _x < 8) { x = _x; } else throw invalid_index; }
+void TCoord::set_y(int _y) { if (_y > -2 && _y < 8) { y = _y; } else throw invalid_index; }
+short int TCoord::get_x() { return x; }
+short int TCoord::get_y() { return y; }
+TCoord& TCoord::operator=(const TCoord& tmp) {
+    x = tmp.x;
+    y = tmp.y;
+    return *this;
+}
+bool TCoord::operator==(const TCoord& tmp) const {
+    return ((x == tmp.x) && (y == tmp.y));
+}
+TCoord::TCoord(short int _x, short int _y) {
+    if (_x < 8 && _x > -2 && _y > -2 && _y < 8) {
+        x = _x;
+        y = _y;
+    }
+    else throw invalid_index;
+}
 ostream& operator<<(ostream& os, const TCoord& coord) {
     os << "X = " << coord.x << "Y = " << coord.y;
     return os;
@@ -120,6 +127,8 @@ protected:
     TFigure_color color;//цвет фигуры
 public:
     TCoord get_coord() { return coord; }
+    TFigure_type get_figure_type() { return type; }
+    TFigure_color get_figure_color() { return color; }
     virtual TCoordMass& get_list_coord() = 0;
     virtual bool check_move(TCoord& coord_last) = 0;
     virtual void move_to(TCoord& coord_last) = 0;
@@ -174,7 +183,6 @@ bool TPawn::check_move(TCoord& coord_last) {
         else
         {
             if ((coord.get_y() == 7) && (coord.get_y() - 2 == coord_last.get_y() && (coord.get_x() == coord_last.get_x()))) return 1; else return 0;
-
         }
     }
     else if (color == white) {
@@ -507,14 +515,14 @@ TKnight::TKnight(short int _id, TCoord _coord, TFigure_color _color) {
 bool TKnight::check_move(TCoord& coord_last) {
     TCoord deth_coord(-1, -1);
     if (coord_last == deth_coord) throw invalid_index;
-    if ((TCoord(coord.get_y() - 2, coord.get_x() - 1) == coord_last) && (coord.get_y() - 2 >= 0 && coord.get_x() - 1 >= 0)) return true;//fghjgh
-    if ((TCoord(coord.get_y() - 2, coord.get_x() + 1) == coord_last) && (coord.get_y() - 2 >= 0 && coord.get_x() + 1 <= 7)) return true;
-    if ((TCoord(coord.get_y() - 1, coord.get_x() + 2) == coord_last) && (coord.get_y() - 1 >= 0 && coord.get_x() + 2 <= 7)) return true;
-    if ((TCoord(coord.get_y() + 1, coord.get_x() + 2) == coord_last) && (coord.get_y() + 1 <= 7 && coord.get_x() + 2 <= 7)) return true;
-    if ((TCoord(coord.get_y() - 1, coord.get_x() - 2) == coord_last) && (coord.get_y() - 1 >= 0 && coord.get_x() - 2 >= 0)) return true;
-    if ((TCoord(coord.get_y() + 1, coord.get_x() - 2) == coord_last) && (coord.get_y() + 1 <= 7 && coord.get_x() - 2 >= 0)) return true;
-    if ((TCoord(coord.get_y() + 2, coord.get_x() - 1) == coord_last) && (coord.get_y() + 2 <= 7 && coord.get_x() - 1 >= 0)) return true;
-    if ((TCoord(coord.get_y() + 2, coord.get_x() + 1) == coord_last) && (coord.get_y() + 2 <= 7 && coord.get_x() + 1 <= 7)) return true;//hhjkhjk
+    if ((coord.get_y() - 2 == coord_last.get_y() && coord.get_x() - 1 == coord_last.get_x()) && (coord.get_y() - 2 >= 0 && coord.get_x() - 1 >= 0)) return true;
+    if ((coord.get_y() - 2 == coord_last.get_y() && coord.get_x() + 1 == coord_last.get_x()) && (coord.get_y() - 2 >= 0 && coord.get_x() + 1 <= 7)) return true;
+    if ((coord.get_y() - 1 == coord_last.get_y() && coord.get_x() + 2 == coord_last.get_x()) && (coord.get_y() - 1 >= 0 && coord.get_x() + 2 <= 7)) return true;
+    if ((coord.get_y() + 1 == coord_last.get_y() && coord.get_x() + 2 == coord_last.get_x()) && (coord.get_y() + 1 <= 7 && coord.get_x() + 2 <= 7)) return true;
+    if ((coord.get_y() - 1 == coord_last.get_y() && coord.get_x() - 2 == coord_last.get_x()) && (coord.get_y() - 1 >= 0 && coord.get_x() - 2 >= 0)) return true;
+    if ((coord.get_y() + 1 == coord_last.get_y() && coord.get_x() - 2 == coord_last.get_x()) && (coord.get_y() + 1 <= 7 && coord.get_x() - 2 >= 0)) return true;
+    if ((coord.get_y() + 2 == coord_last.get_y() && coord.get_x() - 1 == coord_last.get_x()) && (coord.get_y() + 2 <= 7 && coord.get_x() - 1 >= 0)) return true;
+    if ((coord.get_y() + 2 == coord_last.get_y() && coord.get_x() + 1 == coord_last.get_x()) && (coord.get_y() + 2 <= 7 && coord.get_x() + 1 <= 7)) return true;
     return false;
 };
 TCoordMass& TKnight::get_list_coord() {
@@ -545,80 +553,83 @@ class TFigure_mass {
     TFigure** mass;
     short int count;
 public:
-    TFigure* operator[](int i) {
-        if (i >= 0 && i < count) return mass[i];
-        else throw invalid_index;
-    }
-    TFigure* operator[](TCoord coord) {
-        for (int i = 0; i < count; i++) {
-            if (mass[i]->get_coord() == coord) {
-                return mass[i];
-            }
-        }
-        return nullptr;
-    }
-    void add_figure(TFigure* figure) {
-        TFigure** tmp_mass = new TFigure * [count + 1];
-        for (int i = 0; i < count; i++) {
-            tmp_mass[i] = mass[i];
-        }
-        tmp_mass[count] = figure;
-        delete[] mass;//добавить отдельный цикл для удаления
-        mass = tmp_mass;
-        count++;
-    };
-    TFigure_mass() {
-        count = 32;
-        mass = new TFigure * [count];
-        TCoord coord(0, 1);
-        for (short int i = 0; i < 8; i++) {
-            coord.set_x(i);
-            mass[i] = new TPawn(i, coord, white);
-        }
-        coord.set_y(0);
-        coord.set_x(0);
-        mass[8] = new TRook(8, coord, white);
-        coord.set_x(7);
-        mass[9] = new TRook(9, coord, white);
-        coord.set_x(1);
-        mass[10] = new TKnight(10, coord, white);
-        coord.set_x(6);
-        mass[11] = new TKnight(11, coord, white);
-        coord.set_x(2);
-        mass[12] = new TBishop(12, coord, white);
-        coord.set_x(5);
-        mass[13] = new TBishop(13, coord, white);
-        coord.set_x(3);
-        mass[14] = new TQueen(14, coord, white);
-        coord.set_x(4);
-        mass[15] = new TKing(15, coord, white);
-
-        coord.set_y(6);
-        coord.set_x(0);
-        for (short int i = 0; i < 8; i++) {
-            coord.set_x(i);
-            mass[16 + i] = new TPawn(16 + i, coord, black);
-        }
-        coord.set_y(7);
-        coord.set_x(0);
-        mass[24] = new TRook(24, coord, black);
-        coord.set_x(7);
-        mass[25] = new TRook(25, coord, black);
-        coord.set_x(1);
-        mass[26] = new TKnight(26, coord, black);
-        coord.set_x(6);
-        mass[27] = new TKnight(27, coord, black);
-        coord.set_x(2);
-        mass[28] = new TBishop(28, coord, black);
-        coord.set_x(5);
-        mass[29] = new TBishop(29, coord, black);
-        coord.set_x(3);
-        mass[30] = new TQueen(30, coord, black);
-        coord.set_x(4);
-        mass[31] = new TKing(31, coord, black);
-    }
+    TFigure* operator[](int i);
+    TFigure* operator[](TCoord coord);
+    void add_figure(TFigure* figure);
+    TFigure_mass();
 };
+TFigure* TFigure_mass::operator[](int i) {
+    if (i >= 0 && i < count) return mass[i];
+    else throw invalid_index;
+}
+TFigure* TFigure_mass::operator[](TCoord coord) {
+    for (int i = 0; i < count; i++) {
+        if (mass[i]->get_coord() == coord) {
+            return mass[i];
+        }
+    }
+    throw invalid_index;
+}
+void TFigure_mass::add_figure(TFigure* figure) {
+    TFigure** tmp_mass = new TFigure * [count + 1];
+    for (int i = 0; i < count; i++) {
+        tmp_mass[i] = mass[i];
+    }
+    tmp_mass[count] = figure;
+    delete[] mass;//добавить отдельный цикл для удаления
+    mass = tmp_mass;
+    count++;
+}
+TFigure_mass::TFigure_mass() {
+    count = 32;
+    mass = new TFigure * [count];
+    TCoord coord(0, 1);
+    for (short int i = 0; i < 8; i++) {
+        coord.set_x(i);
+        mass[i] = new TPawn(i, coord, white);
+    }
+    coord.set_y(0);
+    coord.set_x(0);
+    mass[8] = new TRook(8, coord, white);
+    coord.set_x(7);
+    mass[9] = new TRook(9, coord, white);
+    coord.set_x(1);
+    mass[10] = new TKnight(10, coord, white);
+    coord.set_x(6);
+    mass[11] = new TKnight(11, coord, white);
+    coord.set_x(2);
+    mass[12] = new TBishop(12, coord, white);
+    coord.set_x(5);
+    mass[13] = new TBishop(13, coord, white);
+    coord.set_x(3);
+    mass[14] = new TQueen(14, coord, white);
+    coord.set_x(4);
+    mass[15] = new TKing(15, coord, white);
 
+    coord.set_y(6);
+    coord.set_x(0);
+    for (short int i = 0; i < 8; i++) {
+        coord.set_x(i);
+        mass[16 + i] = new TPawn(16 + i, coord, black);
+    }
+    coord.set_y(7);
+    coord.set_x(0);
+    mass[24] = new TRook(24, coord, black);
+    coord.set_x(7);
+    mass[25] = new TRook(25, coord, black);
+    coord.set_x(1);
+    mass[26] = new TKnight(26, coord, black);
+    coord.set_x(6);
+    mass[27] = new TKnight(27, coord, black);
+    coord.set_x(2);
+    mass[28] = new TBishop(28, coord, black);
+    coord.set_x(5);
+    mass[29] = new TBishop(29, coord, black);
+    coord.set_x(3);
+    mass[30] = new TQueen(30, coord, black);
+    coord.set_x(4);
+    mass[31] = new TKing(31, coord, black);
+}
 
 
 
@@ -631,15 +642,121 @@ class TGame {
     short int move_count;
     TCoordMass history; //доработать, не готово
 public:
-    bool move(TCoord first_coord, TCoord last_coord);
-    TCoord& get_possible_move(TCoord first_coord);
     TGame();
     TGame(TGame& tmp) {
         //mass = tmp.mass; нет перегрузки = для TFigure_mass, добавть.
         move_count = tmp.move_count;
         history = tmp.history;
     }
+    void move(TCoord first_coord, TCoord last_coord);
+    void eatten(TFigure* figure);
+    bool check_possibility_move(TCoord fitst_coorf, TCoord last_coord);
+    void castling();
+    bool end_game();
 };
+TGame::TGame() {
+    move_count = 0;
+}
+void TGame::move(TCoord first_coord, TCoord last_coord) {
+    if (check_possibility_move(first_coord, last_coord)) {
+        history.add_coord(first_coord);
+        history.add_coord(last_coord);
+        move_count += 1;
+        mass[first_coord]->move_to(last_coord);
+        try {
+            TFigure* figure = mass[last_coord];
+            eatten(figure);
+        }
+        catch (...) {}
+    }
+}
+void TGame::eatten(TFigure* figure) {
+    TCoord death(-1, -1);
+    figure->move_to(death);
+}
+bool TGame::check_possibility_move(TCoord first_coord, TCoord last_coord) {
+    TFigure* figure_1 = mass[first_coord];
+    if (figure_1->get_figure_type() != pawn) {
+        if (figure_1->check_move(last_coord)) {
+            //Тут я проверяю наличие фигур между начальной и конечной координатой
+            TCoordMass list_coord = figure_1->get_list_coord();
+            int k = list_coord.get_count();
+            int difference_x = last_coord.get_x() - first_coord.get_x();
+            int difference_y = last_coord.get_y() - first_coord.get_y();
+            for (int i = 0; i < k; i++) {
+                if (difference_x == 0 && (list_coord[i].get_y() - first_coord.get_y()) / difference_y < 1) {
+                    try {
+                        TFigure* figure_2 = mass[list_coord[i]];
+                        return 0;
+                    }
+                    catch (...) {
+                        continue;
+                    }
+                }
+                if (difference_y == 0 && (list_coord[i].get_x() - first_coord.get_x()) / difference_x < 1) {
+                    try {
+                        TFigure* figure_2 = mass[list_coord[i]];
+                        return 0;
+                    }
+                    catch (...) {
+                        continue;
+                    }
+                }
+                if ((list_coord[i].get_x() - first_coord.get_x()) / difference_x < 1  && 
+                    (list_coord[i].get_x() - first_coord.get_x()) / difference_x == (list_coord[i].get_y() - first_coord.get_y()) / difference_y) {
+                    try {
+                        TFigure* figure_2 = mass[list_coord[i]];
+                        return 0;
+                    }
+                    catch (...) {
+                        continue;
+                    }
+                }
+            }
+            //Если мы ничего не вернули во время цикла, то остается только проверить можем мы ли мы съесть фигуру на данной ячейке
+            try {
+                if (mass[last_coord]->get_figure_color() != figure_1->get_figure_color()) {
+                    return 1;
+                }
+                else { return 0; }
+            }
+            catch (...) {
+                return 1;
+            }
+        }
+    }
+    else {
+        if (figure_1->check_move(last_coord)) {
+            try {
+                TFigure* figure_2 = mass[last_coord];
+                return 0;
+            }
+            catch (...) {
+                return 1;
+            }
+        }
+        else if (abs(first_coord.get_x() - last_coord.get_x()) == 1 && last_coord.get_y() - first_coord.get_y() == 1 && figure_1->get_figure_color() == white) {
+            try {
+                TFigure* figure_2 = mass[last_coord];
+                return 1;
+            }
+            catch (...) {
+                return 0;
+            }
+        }
+        else if (abs(first_coord.get_x() - last_coord.get_x()) == 1 && last_coord.get_y() - first_coord.get_y() == -1 && figure_1->get_figure_color() == black) {
+            try {
+                TFigure* figure_2 = mass[last_coord];
+                return 1;
+            }
+            catch (...) {
+                return 0;
+            }
+        }
+    }
+
+}
+
 
 
 
@@ -651,15 +768,15 @@ public:
 int main()
 {
     //создаем координату
-    TCoord s(0, 0);
-    //s.set_y(0);
-    //s.set_x(0);
+    TCoord s;
+    s.set_y(0);
+    s.set_x(0);
     //вызывается конструктор TPawn::TPawn(short int _id, TCoord _coord, TFigure_color _color)
     //TPawn t1(1, s, white);
-    TRook t2(1, s, white);
+    TKnight t2(1, s, white);
     //меняем координату
-    s.set_x(0);
-    s.set_y(6);
+    s.set_x(1);
+    s.set_y(2);
     //проверка может ли фигура сходить на новую координату
     if (t2.check_move(s)) { cout << "First test: " << "TRUE TEST, check_move return true " << endl; }
     else { cout << "First test: " << "FALSE TEST, check_move return false " << endl; }
